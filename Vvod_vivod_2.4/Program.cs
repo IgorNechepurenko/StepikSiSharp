d90d4816f51428bda50ac27f040f037ad6c73bbf
Igor Nechepurenko <nahalenok33@bk.ru>
2022-06-30 07:45:37 +0800

    dob fail

---
+++ Vvod_vivod_2.4/Program.cs
@@ -0,0 +1,20 @@
+﻿// Console.WriteLine("Hello world!");
+
+// Console.WriteLine("! № 12 34 56 90");
+
+// Console.WriteLine("!"); 
+// Console.WriteLine();
+// Console.WriteLine("№");
+// Console.WriteLine("12");
+// Console.WriteLine("34");
+// Console.WriteLine("56");
+// Console.WriteLine("90");
+
+// Console.Write("!\n\n№\n12\n34\n56\n90\n");
+
+// Console.WriteLine("Введите своё имя:");
+// var name = Console.ReadLine();
+// Console.WriteLine($"Здравствуй, {name}! ");
+
+var name = Console.ReadLine();
+Console.WriteLine($"Лучший фильм 2021 года - {name} ");
