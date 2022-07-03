c157b48f7f66c2d2b5a90855f5146ab0583fcfc1
userName <nahalenok33@bk.ru>
2022-07-03 14:31:07 +0800

    kalkulator_okruglenie

--- Osnovi_programmir/Program.cs
+++ Osnovi_programmir/Program.cs
@@ -1,23 +1,23 @@
 ﻿// Конвертор валют, рубля к доллару:
 
-// Console.WriteLine("Введите сумму в рублях:");
-// var rubles = Convert.ToDouble(Console.ReadLine());
-// double dollar = 54.8;
-// double euro = 58.4;
-// var RubToDollar = rubles/dollar;
-// var RubToEuro = rubles/euro;
-// Console.WriteLine(rubles + " рублей = " + RubToDollar + " - " + "доллар");
-// Console.WriteLine(rubles + " рублей = " + RubToEuro + " - " + "евро");
+Console.WriteLine("Введите сумму в рублях:");
+var rubles = Convert.ToDouble(Console.ReadLine());
+double dollar = 54.8;
+double euro = 57.4;
+var RubToDollar = Math.Round(rubles/dollar, 2);
+var RubToEuro = Math.Round(rubles/euro, 2);
+Console.WriteLine(rubles + " рублей = " + RubToDollar + " - " + "доллар");
+Console.WriteLine(rubles + " рублей = " + RubToEuro + " - " + "евро");
 
 // Подсчитать площадь трапеции: 
 
-Console.WriteLine("Введите длинну верхнего основания");
-double a = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введите длинну нижнего основания");
-double b = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введите высоту");
-double h = Convert.ToDouble(Console.ReadLine());
-var s = (a+b) / 2 * h;
-Console.WriteLine("Площадь трапеции = " + s);
+// Console.WriteLine("Введите длинну верхнего основания");
+// double a = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите длинну нижнего основания");
+// double b = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите высоту");
+// double h = Convert.ToDouble(Console.ReadLine());
+// var s = (a+b) / 2 * h;
+// Console.WriteLine("Площадь трапеции = " + s);
 
 
