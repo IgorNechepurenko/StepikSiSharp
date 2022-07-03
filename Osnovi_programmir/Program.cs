f0abf5b580ef393e4505fc922ced00d1cb6616d8
userName <nahalenok33@bk.ru>
2022-07-03 10:13:31 +0800

    converter money

---
+++ Osnovi_programmir/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите сумму в рублях:");
+var rubles = Convert.ToDouble(Console.ReadLine());
+double dollar = 54.8;
+double euro = 58.4;
+var RubToDollar = rubles/dollar;
+var RubToEuro = rubles/euro;
+Console.WriteLine(rubles + " рублей = " + RubToDollar + " - " + "доллар");
+Console.WriteLine(rubles + " рублей = " + RubToEuro + " - " + "евро");
