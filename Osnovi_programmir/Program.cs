c2ad7b36c09aa5cb6810d5641f2114048ce6eddb
userName <nahalenok33@bk.ru>
2022-07-03 10:39:34 +0800

    trapezia

--- Osnovi_programmir/Program.cs
+++ Osnovi_programmir/Program.cs
@@ -1,8 +1,23 @@
-﻿Console.WriteLine("Введите сумму в рублях:");
-var rubles = Convert.ToDouble(Console.ReadLine());
-double dollar = 54.8;
-double euro = 58.4;
-var RubToDollar = rubles/dollar;
-var RubToEuro = rubles/euro;
-Console.WriteLine(rubles + " рублей = " + RubToDollar + " - " + "доллар");
-Console.WriteLine(rubles + " рублей = " + RubToEuro + " - " + "евро");
+﻿// Конвертор валют, рубля к доллару:
+
+// Console.WriteLine("Введите сумму в рублях:");
+// var rubles = Convert.ToDouble(Console.ReadLine());
+// double dollar = 54.8;
+// double euro = 58.4;
+// var RubToDollar = rubles/dollar;
+// var RubToEuro = rubles/euro;
+// Console.WriteLine(rubles + " рублей = " + RubToDollar + " - " + "доллар");
+// Console.WriteLine(rubles + " рублей = " + RubToEuro + " - " + "евро");
+
+// Подсчитать площадь трапеции: 
+
+Console.WriteLine("Введите длинну верхнего основания");
+double a = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите длинну нижнего основания");
+double b = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите высоту");
+double h = Convert.ToDouble(Console.ReadLine());
+var s = (a+b) / 2 * h;
+Console.WriteLine("Площадь трапеции = " + s);
+
+
