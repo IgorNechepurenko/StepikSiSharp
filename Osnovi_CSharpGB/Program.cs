7a3375491b9e25f7b03189e683f48802b9037ffe
userName <nahalenok33@bk.ru>
2022-07-04 18:17:00 +0800

    film

---
+++ Osnovi_CSharpGB/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine( "Эта программа поможет найти фильм, который будет вам по душе." );
+Console.WriteLine("Как вас зовут?");
+String name = Console.ReadLine();
+			
+Console.WriteLine( "Здравствуте, " + name );
+
+Console.WriteLine( "Фильм какого жанра вы бы хотели посмотреть: комедия или драма" );
+String genre = Console.ReadLine();
+
+Console.WriteLine( "В таком случае, рекомендую посмотреть вам следуюдие фильмы:" );
+
+if (genre == "комедия" )
+	{				
+	Console.WriteLine( "* Иван Васильевич меняет профессию" );
+	Console.WriteLine( "* Операция «Ы» и другие приключения Шурика" );
+	Console.WriteLine( "* Назад в будущее" );
+	Console.WriteLine( "* В джазе только девушки" );
+	}
+	else
+	{
+	Console.WriteLine( "* Зеленая миля" );
+	Console.WriteLine( "* Побег из Шоушенка" );
+	}
+
+Console.ReadLine();
