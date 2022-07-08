26cc0d51e6dfc2d1b6be245f7911b07e35c8cc23
userName <nahalenok33@bk.ru>
2022-07-08 15:53:50 +0800

    01

--- Osnovi_CSharpGB/Program.cs
+++ Osnovi_CSharpGB/Program.cs
@@ -24,17 +24,17 @@
 
 // Console.ReadLine();
 
-string a = Console.ReadLine();
-string b = Console.ReadLine();
+// string a = Console.ReadLine();
+// string b = Console.ReadLine();
 
-int num1 = Int32.Parse(a);
-int num2 = Int32.Parse(b);
+// int num1 = Int32.Parse(a);
+// int num2 = Int32.Parse(b);
 
-int result = num1 + num2;
-Console.WriteLine(result);
-result = num1 * num2;
-Console.WriteLine(result);
-double average = (double)(num1 + num2) / 2;
-Console.WriteLine(average);
+// int result = num1 + num2;
+// Console.WriteLine(result);
+// result = num1 * num2;
+// Console.WriteLine(result);
+// double average = (double)(num1 + num2) / 2;
+// Console.WriteLine(average);
 
-Console.ReadLine();
+// Console.ReadLine();
