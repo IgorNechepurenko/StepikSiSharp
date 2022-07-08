bf7509b8cc46bcb25f9fa7bb03032bc0e17d6ba8
userName <nahalenok33@bk.ru>
2022-07-08 15:48:41 +0800

    01

--- DZ_GB_1/Program.cs
+++ DZ_GB_1/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Напишите программу, которая на вход принимает три числа,
+// и выдаёт максимальное из этих чисел.
+
+// Console.WriteLine("Введите три  числа:_");
+// int a = int.Parse(Console.ReadLine());
+// int b = int.Parse(Console.ReadLine());
+// int c = int.Parse(Console.ReadLine());
+// int max = a;
+// if (a > max) max = a;
+// if (b > max) max = b;
+// if (c > max) max = c;
+// Console.WriteLine("Максимальное число = " + max);
+
+// Напишите программу, которая на вход принимает два числа, а на выходе выдаёт 
+// какое число больше, а кокое меньше.
+ 
+//  Console.WriteLine("Введите два  числа:_");
+// var a = int.Parse(Console.ReadLine());
+// var b = int.Parse(Console.ReadLine());
+// if (a > b) Console.WriteLine("Максимальное число = " + a);
+// else
+// {
+//    Console.WriteLine("Максимальное число = " + b); 
+// }
+
+// Напишите программу, которая на вход принимает число и выдаёт 
+// является оно чётным или нет.
+
+// Console.WriteLine("Введите число:_");
+// var num = int.Parse(Console.ReadLine());
+// if (num % 2==0) Console.WriteLine("Число: " +num+ "- четное");
+// else Console.WriteLine("Число: " +num+ " - не четное");
+
+// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите любое число:_");
+int num;
+num = Convert.ToInt32(Console.ReadLine());
+
+
+
