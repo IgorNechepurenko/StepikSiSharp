a5597dad2a0a79a1716a0dc21ff4a0094e35a51a
userName <nahalenok33@bk.ru>
2022-07-08 19:59:26 +0800

    DZ1_4Z

--- DZ_GB_1/Program.cs
+++ DZ_GB_1/Program.cs
@@ -36,8 +36,11 @@
 // а на выходе показывает все чётные числа от 1 до N.
 
 Console.WriteLine("Введите любое число:_");
-int num;
-num = Convert.ToInt32(Console.ReadLine());
-
+int N;
+N = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= N; i++){
+    if(i%2==0)
+    Console.Write(i+" ");
+}
 
 
