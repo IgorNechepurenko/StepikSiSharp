98c6048b53b90784d3a6ef6d99b5d48ac1ae75b9
userName <nahalenok33@bk.ru>
2022-07-14 18:24:24 +0800

    DZ3

--- DZ_GB_1/Program.cs
+++ DZ_GB_1/Program.cs
@@ -85,20 +85,63 @@
 // Напишите программу, которая принимает на вход пятизначное число и проверяет,
 // является ли оно палиндромом.
 
-Console.Write("Введи пятизначное число: ");
-string? number = Console.ReadLine();
+// Console.Write("Введи пятизначное число: ");
+// string? number = Console.ReadLine();
 
-void num(string number){
-  if (number[0]==number[4] || number[1]==number[3]){
-    Console.WriteLine("Число: " +number + " палиндром.");
+// void num(string number){
+//   if (number[0]==number[4] || number[1]==number[3]){
+//     Console.WriteLine("Число: " +number + " - палиндром.");
+//   }
+//   else Console.WriteLine("Число: " +number + " - не палиндром.");
+// }
+
+// if (number!.Length == 5){
+//   num(number);
+// }
+// else Console.WriteLine("Введи правильное число!");
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит 
+// расстояние между ними в 3D пространстве.
+
+
+// Console.WriteLine("Введите 6 цифр координат 2-х точек:_");
+// double x, y, z, x1, y1, z1, L;
+// x = Convert.ToDouble(Console.ReadLine());
+// y = Convert.ToDouble(Console.ReadLine());
+// z = Convert.ToDouble(Console.ReadLine());
+// x1 = Convert.ToDouble(Console.ReadLine());
+// y1 = Convert.ToDouble(Console.ReadLine());
+// z1 = Convert.ToDouble(Console.ReadLine());
+// L = Math.Sqrt((x-x1)*(x-x1)+(y-y1)*(y-y1)+(z-z1)*(z-z1));
+// Console.WriteLine("Расстояние между точками= : " + L);
+
+// Напишите программу, которая принимает на вход число (N) и 
+// выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cube){
+  int counter = 0;
+  int length = cube.Length;
+  while (counter <  length){
+    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+    counter++;
   }
-  else Console.WriteLine("Число: " +number + " не палиндром.");
 }
+void PrintArry(int[] coll){
+  int count = coll.Length;
+  int index = 1;
+  while(index < count){
+    Console.Write(coll[index]+ " ");
+    index++;
+  }
+} 
+int[] arry = new int[cube+1];
+Cube(arry);
+PrintArry(arry);
 
-if (number!.Length == 5){
-  num(number);
-}
-else Console.WriteLine("Введи правильное число!");
+
 
 
 
