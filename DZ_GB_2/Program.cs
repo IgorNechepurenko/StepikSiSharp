142b4643ce0d7bbb5739b9c9ff048c65d17c2ecd
userName <nahalenok33@bk.ru>
2022-07-23 12:19:00 +0800

    01

--- DZ_GB_2/Program.cs
+++ DZ_GB_2/Program.cs
@@ -77,45 +77,82 @@
 // и минимальным элементов массива.
 
 
-Console.WriteLine("Введите размер массива: ");
-int a = Convert.ToInt32(Console.ReadLine());
-double[] num = new double[a];
-FillArrayRandomNumbers(num);
-Console.WriteLine("Массив: ");
-PrintArray(num);
-double min = Int32.MaxValue;
-double max = Int32.MinValue;
+// Console.WriteLine("Введите размер массива: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// double[] num = new double[a];
+// FillArrayRandomNumbers(num);
+// Console.WriteLine("Массив: ");
+// PrintArray(num);
+// double min = Int32.MaxValue;
+// double max = Int32.MinValue;
 
-for (int b = 0; b < num.Length; b++)
-{
-    if (num[b] > max)
-        {
-            max = num[b];
-        }
-    if (num[b] < min)
-        {
-            min = num[b];
-        }
-}
+// for (int b = 0; b < num.Length; b++)
+// {
+//     if (num[b] > max)
+//         {
+//             max = num[b];
+//         }
+//     if (num[b] < min)
+//         {
+//             min = num[b];
+//         }
+// }
 
-Console.WriteLine($"Всего {num.Length} чисел.");
-Console.WriteLine($"Максимальное значение: {max}, минимальное значение: {min}");
-Console.WriteLine($"Разница между {max} и {min}, составляет: {max - min}");
+// Console.WriteLine($"Всего {num.Length} чисел.");
+// Console.WriteLine($"Максимальное значение: {max}, минимальное значение: {min}");
+// Console.WriteLine($"Разница между {max} и {min}, составляет: {max - min}");
 
-void FillArrayRandomNumbers(double[] num)
-{
-    for(int i = 0; i < num.Length; i++)
-        {
-            num[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
-        }
-}
-void PrintArray(double[] num)
-{
-    Console.Write("[ ");
-    for(int i = 0; i < num.Length; i++)
-        {
-            Console.Write(num[i] + " ");
-        }
-    Console.Write("]");
-    Console.WriteLine();
-}
+// void FillArrayRandomNumbers(double[] num)
+// {
+//     for(int i = 0; i < num.Length; i++)
+//         {
+//             num[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+//         }
+// }
+// void PrintArray(double[] num)
+// {
+//     Console.Write("[ ");
+//     for(int i = 0; i < num.Length; i++)
+//         {
+//             Console.Write(num[i] + " ");
+//         }
+//     Console.Write("]");
+//     Console.WriteLine();
+// }
+
+
+//                          41
+// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько 
+// чисел больше 0 ввёл пользователь.
+
+// Console.Write("Введите несколько чисел: ");
+// int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+// int count = 0;
+ 
+// for (int i = 0; i < arr.Length; i++)
+// {
+//     if (arr[i] > 0) 
+//         count++;
+// }
+// Console.WriteLine($"Количество чисел больше нуля: {count}");
+
+// Напишите программу, которая найдёт точку пересечения двух прямых, заданных 
+// уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются
+//пользователем. b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+// Console.Write("Введите k1: ");
+// var k1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите b1: ");
+// var b1 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите k2: ");
+// var k2 = Convert.ToDouble(Console.ReadLine());
+// Console.Write("Введите b2: ");
+// var b2 = Convert.ToDouble(Console.ReadLine());
+  
+// var x = -(b1 - b2) / (k1 - k2);
+// var y = k1 * x + b1;
+ 
+// x = Math.Round(x, 3);
+// y = Math.Round(y, 3);
+ 
+// Console.WriteLine($"Пересечение в точке: ({x};{y})");
