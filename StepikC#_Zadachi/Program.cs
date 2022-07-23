3990bda78b9ffb635cfe0f12109ad161394529b9
userName <nahalenok33@bk.ru>
2022-07-23 12:17:55 +0800

    add Zadachi

---
+++ StepikC#_Zadachi/Program.cs
@@ -0,0 +1,16 @@
+﻿// Console.WriteLine("Введи число");
+// for (int a = Convert.ToInt32(Console.ReadLine()); a>0; a-- )
+// Console.WriteLine("Ошибка!");
+
+// Console.WriteLine("Введите число 1:");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число 2:");
+// int b = Convert.ToInt32(Console.ReadLine());
+// // for (int i = 0; i<a; i++)
+// for (int i = 0; i<a; i++, b++) // второй счётчик
+// {
+// //   int c = b * 2;
+// //   Console.WriteLine( $"{b} * 2 = {c}");
+// //   b++;
+//  Console.WriteLine($"{b} * {2} = {b * 2}");
+// }
