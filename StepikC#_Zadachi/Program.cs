66032a8ad1409fde72097602eee3cbcd76d93893
userName <nahalenok33@bk.ru>
2022-07-25 13:35:18 +0800

    calculator

--- StepikC#_Zadachi/Program.cs
+++ StepikC#_Zadachi/Program.cs
@@ -25,25 +25,87 @@
 //         Console.Write(i + " ");
 //     }
 
-// Блокнот
+//               Блокнот
 
-Console.Write("Введите имя: ");
-string name = Console.ReadLine();
-Console.WriteLine("Имя ученика: " + name);
+// Console.Write("Введите имя: ");
+// string name = Console.ReadLine();
+// Console.WriteLine("Имя ученика: " + name);
 
-Console.Write("Введите возраст: ");
-int age = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Возраст ученика: " + age);
+// Console.Write("Введите возраст: ");
+// int age = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Возраст ученика: " + age);
 
-Console.Write("Введите балл по русскому языку: ");
+// Console.Write("Введите балл по русскому языку: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите балл по математике: ");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите балл по информатике: ");
+// int c  = Convert.ToInt32(Console.ReadLine());
+
+// var f = (a+b+c)/3;
+
+// Console.WriteLine("Средний балл ученика: " + f);
+
+// Калькулятор
+
+Console.WriteLine("Нажмите 1 если хотите выполнить сложение");
+Console.WriteLine("Нажмите 2 если хотите выполнить вычитание");
+Console.WriteLine("Нажмите 3 если хотите выполнить умножение");
+Console.WriteLine("Нажмите 4 если хотите выполнить деление");
+Console.WriteLine("Нажмите 5 если хотите найти остаток от деления");
+
 int a = Convert.ToInt32(Console.ReadLine());
+if (a==1)
+{
+    Console.Write("Введите слагаемое 1: ");
+    int sl1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите слагаемое 2: ");
+    int sl2 = Convert.ToInt32(Console.ReadLine());
+    int a1 = sl1+sl2;
+    Console.WriteLine("Сумма чисел = " + a1);
+}
+else if (a==2) 
+{
+    Console.Write("Введите уменьшаемое 1: ");
+    int sl1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите вычитаемое 2: ");
+    int sl2 = Convert.ToInt32(Console.ReadLine());
+    int a1 = sl1-sl2;
+    Console.WriteLine("Разность чисел = " + a1);
+}
+else if (a==3) 
+{
+    Console.Write("Введите множитель 1: ");
+    int sl1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите множитель 2: ");
+    int sl2 = Convert.ToInt32(Console.ReadLine());
+    int a1 = sl1*sl2;
+    Console.WriteLine("Произведение чисел = " + a1);
+}
+else if (a==4) 
+{
+    Console.Write("Введите делимое 1: ");
+    int sl1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите делитель 2: ");
+    int sl2 = Convert.ToInt32(Console.ReadLine());
+    int a1 = sl1/sl2;
+    Console.WriteLine("Частное чисел = " + a1);
+}
+else if (a==5) 
+{
+    Console.Write("Введите делимое 1: ");
+    int sl1 = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите делитель 2: ");
+    int sl2 = Convert.ToInt32(Console.ReadLine());
+    int a1 = sl1%sl2;
+    Console.WriteLine("Остаток от деления = " + a1);
+}
+else 
+{
+Console.WriteLine("Значение некорректно");
+}
 
-Console.Write("Введите балл по математике: ");
-int b = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите балл по информатике: ");
-int c  = Convert.ToInt32(Console.ReadLine());
-
-var f = (a+b+c)/3;
-
-Console.WriteLine("Средний балл ученика: " + f);
+        
